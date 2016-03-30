d4f8ed66b303d3e49ceb0789252b4ec2696bbfbd
Joonas Haaparanta <joonas.haaparanta@sysart.fi>
2016-03-30 15:11:43 +0300

    Logo hover

--- src/components/CustomerIcons/customerLogo.js
+++ src/components/CustomerIcons/customerLogo.js
@@ -5,17 +5,47 @@ export class CustomerLogo extends Component {
 
     constructor(props, context){
         super(props, context);
+
+        this.state = {
+            hover: true
+        }
     }
 
+    onMouseEnter = () => {
+        this.setState({
+            hover: true
+        });
+    };
+
+    onMouseLeave = () => {
+        this.setState({
+            hover: false
+        });
+    };
+
     render() {
-        const { logo } = this.props.item;
+        const { logo, logo_hover } = this.props.item;
         console.log('Customer logo', this.props);
 
-        let postImage = '';
+        let image = (
+            <img src={logo} />
+        );
+
+        if(this.state.hover){
+            if(logo_hover){
+                image = (<img src={logo_hover} />)
+            }
+
+        }
+
 
         return (
-            <div className="col-xs-12 col-sm-6 col-md-4 col-lg-4 text-center customer-logo">
-                <img src={logo} />
+            <div
+                className="col-xs-12 col-sm-6 col-md-4 col-lg-4 text-center customer-logo"
+                onMouseEnter={this.onMouseEnter}
+                onMouseLeave={this.onMouseLeave}
+            >
+                {image}
             </div>
         )
     }
