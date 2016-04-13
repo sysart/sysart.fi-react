c4c903979fea1c034e35e0eb1d8629ef7b73fae9
Joonas Haaparanta <joonas.haaparanta@sysart.fi>
2016-04-13 19:52:15 +0300

    FP CTA

--- src/config/rest.js
+++ src/config/rest.js
@@ -1,4 +1,10 @@
 
+const domain = 'http://185.26.50.90:8080/';
+const path = 'wp-json/sysart-extend/';
+const version = 'v1';
+
 export const RestConfig = {
-    domain : 'http://185.26.50.90:8080/'
+    domain : 'http://185.26.50.90:8080/',
+    version : 'wp-json/sysart-extend/v1/',
+    url : domain + path + version
 }
