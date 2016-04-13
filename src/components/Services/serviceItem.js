3e287ab908fc3341df3412e3be39f1e145f6517f
Joonas Haaparanta <joonas.haaparanta@sysart.fi>
2016-04-13 18:21:43 +0300

    FP service as link

--- src/components/Services/serviceItem.js
+++ src/components/Services/serviceItem.js
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import moment from 'moment';
+import { Link } from 'react-router';
 
 export class ServiceItem extends Component {
 
@@ -15,11 +16,13 @@ export class ServiceItem extends Component {
 
         return (
             <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6 text-center">
-                <div className="image-wrapper">
-                    <img src={image} />
-                </div>
-                <h4>{title}</h4>
-                <div dangerouslySetInnerHTML={{__html: description}} />
+                <Link to="/palvelut" activeClassName="active">
+                    <div className="image-wrapper">
+                        <img src={image} />
+                    </div>
+                    <h4>{title}</h4>
+                    <div dangerouslySetInnerHTML={{__html: description}} />
+                </Link>
             </div>
         )
     }
